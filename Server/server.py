c1a3c5465296e619750586526909586a7e695759
pbylicki <bylak@poczta.fm>
2016-01-07 20:51:59 +0100

    Add search controller and some mock data to server

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+from flask import Flask, request
 from flask.ext.cors import cross_origin
 import json
 from time import sleep
@@ -6,11 +6,37 @@ from time import sleep
 
 app = Flask(__name__)
 
-@app.route("/search/:query")
-@cross_origin()
-def search(query):
-    # TODO create some file with data to search from
-    return query
+results = [
+            {
+              "url": "https://pl.wikipedia.org/wiki/EB_%28piwo%29",
+              "title": "EB (piwo) - Wikipedia",
+              "content": "EB Specjal Pils – popularna w latach 90. XX wieku marka jasnego piwa produkowana w browarach w Elblągu i Braniewie."
+            },
+            {
+              "url": "http://wiadomosci.onet.pl/olsztyn/piwo-eb-wraca-na-polski-rynek-to-odpowiedz-na-oferte-browaru-w-braniewie/096djl",
+              "title": "Piwo EB wraca na polski rynek.",
+              "content": "Piwo EB wraca na polski rynek. To odpowiedź na ofertę browaru w Braniewie. Słynne w Elblągu, regionie i całej Polsce..."
+            },
+            {
+              "url": "http://natemat.pl/140833,legendarne-piwo-eb-wraca-na-rynek",
+              "title": "Legendarne piwo EB wraca na rynek.",
+              "content": "Legendarne piwo EB - symbol lat 90. wraca do sklepów. Już pod koniec kwietnia kupimy je w niemal identycznym opakowaniu..."
+            },
+            {
+              "url": "https://www.facebook.com/PiwoEB/",
+              "title": "EB Wróć",
+              "content": "Polub EB Wróć, jeśli pamiętasz smak złotego trunku. Jest to kompletnie niekomercyjny fanklub piwa EB. Na zdrowie!"
+            }
+          ]
+
+@app.route("/search/", methods=['POST', 'OPTIONS'])
+@cross_origin(allow_headers=['Content-Type'])
+def post_search():
+    search_data = request.get_json()
+    query = search_data["query"]
+    # search for query in titles
+    search_results = [result for result in results if query in result["title"].lower()]
+    return json.dumps(search_results, ensure_ascii=False)
 
 @app.route("/contest/", methods=['POST', 'OPTIONS'])
 @cross_origin(allow_headers=['Content-Type'])
